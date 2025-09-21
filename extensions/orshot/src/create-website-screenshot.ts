5b0f37986bd8f1b04c9c2dac879eeead9f3737fa
Rishi Mohan <iamrishi.ms@gmail.com>
2025-09-21 16:33:44 +0530

    Update extensions/orshot/src/create-website-screenshot.ts
    Co-authored-by: Syed M Mokaarim Bin Razi <30526133+xmok@users.noreply.github.com>

--- extensions/orshot/src/create-website-screenshot.ts
+++ extensions/orshot/src/create-website-screenshot.ts
@@ -10,7 +10,7 @@ interface ApiResponse {
   error?: string;
 }
 
-export default async function Command(props: LaunchProps) {
+export default async function Command(props: LaunchProps<{arguments: Arguments.CreateWebsiteScreenshot}>) {
   let { websiteUrl } = props.arguments;
 
   if (!websiteUrl || websiteUrl.trim() === "") {
